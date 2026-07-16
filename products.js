c5283997c23181aefd1a494c411a138434fdcc62
PVithesh <pottabathinivithesh@gmail.com>
2026-07-16 12:18:57 +0530

    Created NavBar

---
+++ products.js
@@ -0,0 +1,16 @@
+//* Fetch Address Functionality
+let userLocation = document.getElementById("location");
+userLocation.addEventListener("click",()=>{
+    userLocation.innerHTML = "";
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        let locationApi = ` https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=jsonv2 `;
+        let fetchingArea = async() => {
+            let respose = await fetch(locationApi)
+            let{address:{suburb,city}} = await respose.json()
+            userLocation.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${suburb} ${city}`
+        }
+        fetchingArea();
+    })
+})
