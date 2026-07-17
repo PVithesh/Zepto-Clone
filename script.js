f1fd85e7b404b12cfc5d66a604e115834c21aa1e
PVithesh <pottabathinivithesh@gmail.com>
2026-07-17 22:38:32 +0530

    Categories images added

--- script.js
+++ script.js
@@ -16,17 +16,42 @@ userLocation.addEventListener("click",()=>{
 })
 
 //* Fetching Categories
-let fetchCategories = async()=>{
-    let respose = await fetch("https://dummyjson.com/products/categories");
-    let categories = await respose.json();
-    console.log(categories);
-    let categoryItem = document.getElementById("category-items");
-    categories.forEach((category)=>{
-        categoryItem.innerHTML += `
-        <div class="category-card">
-                <div class="category-img">🤍</div>
+// let fetchCategories = async()=>{
+//     let respose = await fetch("https://dummyjson.com/products/categories");
+//     let categories = await respose.json();
+//     console.log(categories);
+//     let categoryItem = document.getElementById("category-items");
+//     categories.forEach((category)=>{
+//         categoryItem.innerHTML += `
+//         <div class="category-card">
+//                 <div class="category-img">🤍</div>
+//                 <p class="category-name">${category.name}</p>
+//         </div>`        
+//     })
+// }
+// fetchCategories();
+
+//* Fetch Categories and their Thumbnail Images
+
+let fetchCategories = async () => {
+    //^ Fetch Categories
+    let categoryResponse = await fetch("https://dummyjson.com/products/categories");
+    let categories = await categoryResponse.json();
+    //^ Fetch All Products
+    let productResponse = await fetch("https://dummyjson.com/products?limit=0");
+    let productData = await productResponse.json();
+    let categoryItems = document.getElementById("category-items");
+    categories.forEach((category) => {
+        //^ Find the first product of this category
+        let product = productData.products.find(
+            (p) => p.category === category.slug
+        );
+        categoryItems.innerHTML += `
+            <div class="category-card">
+                <img src="${product.thumbnail}" alt="${category.name}" class="category-img">
                 <p class="category-name">${category.name}</p>
-        </div>`        
-    })
-}
+            </div>
+        `;
+    });
+};
 fetchCategories();
