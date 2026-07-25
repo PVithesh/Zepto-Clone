f2bd38f2e50b5d8321e38b82c844999cd6558c4e
PVithesh <pottabathinivithesh@gmail.com>
2026-07-25 22:51:27 +0530

    location in categories

--- category.js
+++ category.js
@@ -1,3 +1,20 @@
+//* Fetch Address Functionality
+let userLocation = document.getElementById("location");
+userLocation.addEventListener("click",()=>{
+    userLocation.innerHTML = "Fetching Location..";
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        let locationApi = ` https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=jsonv2 `;
+        let fetchingArea = async() => {
+            let respose = await fetch(locationApi)
+            let{address:{suburb,city}} = await respose.json()
+            userLocation.innerHTML = `<i class="fa-solid fa-location-dot"></i> ${suburb} ${city}`
+        }
+        fetchingArea();
+    })
+})
+
 //!Fetching Individual Category Data
 let categoryDatas = JSON.parse(localStorage.getItem("clickedCategoryData"))
 let categoryName = sessionStorage.getItem("clickedCategory")
