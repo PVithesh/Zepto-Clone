2a4fe8fda5813b382146ba045a2329fb091eba1b
PVithesh <pottabathinivithesh@gmail.com>
2026-07-25 11:45:24 +0530

    Cart functions

--- Cart.js
+++ Cart.js
@@ -8,3 +8,60 @@ function saveCartItems(cartItems){
     return localStorage.setItem(CART_KEY,JSON.stringify(cartItems));
 }
 
+function addToCart(product){
+    let cartItems = getCartItems();
+    let existingProduct = cartItems.find(item =>{
+        return item.id === product.id;
+    })
+    if(existingProduct){
+        existingProduct.qty++;
+    }
+    else{
+        cartItems.push({
+            id : product.id,
+            title : product.title,
+            qty : 1,
+            price : product.price,
+            img : product.thumbnail
+        })
+    }
+    saveCartItems(cartItems);
+}
+
+function incrementQuantity(productId){
+    let cartItems = getCartItems()
+    let product = cartItems.find(item => {
+        return item.id === productId;
+    })
+    if(product){
+        product.qty++
+    }
+    saveCartItems(cartItems);
+}
+
+function decrementQuantity(productId){
+    let cartItems = getCartItems()
+    let product = cartItems.find(item => {
+        return item.id === productId;
+    })
+    if(product){
+        product.qty--
+        if(product.qty === 0){
+            cartItems = cartItems.filter((item) =>{
+                return item.id !== productId
+            })
+        }
+    }
+    saveCartItems(cartItems);
+}
+
+function getProductQuantity(productId){
+    let cartItems = getCartItems()
+    let product = cartItems.find(item=>{
+        return item.id === productId;
+    })
+    if(product){
+        return product.qty;
+    }
+    return 0;
+}
